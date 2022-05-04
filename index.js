3f9a3a0884e6d06f4d509070696782da6752f617
muharemax <muharem.corbadzic@gmail.com>
2022-05-04 19:50:14 +0200

    commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 4545;
+
+app.get('/api/health', (req, res) => 
+  	res.json({ 'status': 'ok' }));
+
+app.get('/api/mirror', function(req, res){
+    res.json({ 'transformed': helper_function ( req.query.word ) });
+});
+
+function helper_function(string) {
+	var newString = x = '';
+	for (var i = 0; i < string.length; i++) {
+		if( !isNaN( string[i] ) ){
+			newString += 10 - string[i]*1 - 1;
+		}else{
+			newString += string[i] + x === string[i].toUpperCase() ? string[i].toLowerCase() : string[i].toUpperCase();
+		}
+	}
+	return newString.split("").reverse().join("");
+}
+
+/* app.listen( port, function () {
+    console.log("App listening on " + port + " port");
+}); */
+
+/* module.exports = helper_function; */
+module.exports = app
