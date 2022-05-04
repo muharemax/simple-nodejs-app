47307ec6a0cea38f6d1a6c189fdf923ec601ce6b
muharemax <muharem.corbadzic@gmail.com>
2022-05-04 20:01:25 +0200

    commit

--- index.test.js
+++ index.test.js
@@ -2,7 +2,15 @@ const app = require('./index');
 const supertest = require('supertest')
 const request = supertest(app)
 
-it('App should return 47RAbOoF for word fOoBar25', async () => {
+it('GET /api/health returns { "status":"ok" }', async () => {
+    request
+        .get('/api/health')
+        .then(res => {
+            expect(res.body.status).toBe('ok')
+        });
+})
+
+it('GET /api/miror?word=47RAbOoF returns { "transformed":"fOoBar25" }', async () => {
     request
         .get('/api/mirror')
         .query({word:'fOoBar25'})
